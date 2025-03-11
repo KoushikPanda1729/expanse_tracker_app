16189dc5e60bfebaf5a3d223bc36855e7fdb8161
Manab Pratim Maity <manab2001maity@gmail.com>
2025-03-11 21:41:49 +0530

    cors added

--- server/main.go
+++ server/main.go
@@ -6,7 +6,9 @@ import (
 	"expanse-tracker/routes"
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,6 +30,17 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
